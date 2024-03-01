1926a768380b75f80218022932ff95bf54eac4d4
auguzinzin <crespin.augustin@gmail.com>
2024-03-01 02:40:43 +0100

    first implem of Lexer

--- project/src/main/java/compiler/Lexer/Symbol.java
+++ project/src/main/java/compiler/Lexer/Symbol.java
@@ -1,4 +1,37 @@
 package compiler.Lexer;
 
 public class Symbol {
+	
+	private String token; 
+	private String attribute;
+	
+	public Symbol(String token, String attribute) {
+		super();
+		this.token = token;
+		this.attribute = attribute;
+	}
+
+	protected String getToken() {
+		return token;
+	}
+
+	protected void setToken(String token) {
+		this.token = token;
+	}
+
+	protected String getAttribute() {
+		return attribute;
+	}
+
+	protected void setAttribute(String attribute) {
+		this.attribute = attribute;
+	}	
+	
+	protected boolean isNull() {
+		return (this.attribute == null && this.token == null);
+	}
+	
+	
 }
+
+
